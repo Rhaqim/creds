180757f113edee8c8e7c5eb0d09590b18236fb5a
Rhaqim <anusiemj@gmail.com>
2024-04-27 19:31:46 +0100

    oauth2 routes

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -1,1 +1,35 @@
 package api
+
+import (
+	"github.com/Rhaqim/creds/internal/authentication"
+	"github.com/gin-gonic/gin"
+)
+
+func LoginHandler(c *gin.Context) {
+	// Retrieve the dynamic value from the context
+	provider := c.Param("provider")
+
+	handler := authentication.NewOAuth2Adapter(provider)
+	if handler == nil {
+		c.AbortWithStatusJSON(400, gin.H{
+			"error": "Invalid provider, valid providers are google and github",
+		})
+		return
+	}
+
+	handler.OAuth2LoginHandler(c)
+}
+
+func CallbackHandler(c *gin.Context) {
+	provider := c.Param("provider")
+
+	handler := authentication.NewOAuth2Adapter(provider)
+	if handler == nil {
+		c.AbortWithStatusJSON(400, gin.H{
+			"error": "Invalid provider, valid providers are google and github",
+		})
+		return
+	}
+
+	handler.OAuth2CallbackHandler(c)
+}
--- internal/web/router/route.go
+++ internal/web/router/route.go
@@ -1,9 +1,18 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Rhaqim/creds/internal/api"
+	"github.com/gin-gonic/gin"
+)
 
 func Init() error {
 	r := gin.Default()
+
+	authentication := r.Group("/auth")
+	{
+		authentication.POST("/:provider/login", api.LoginHandler)
+		authentication.POST("/:provider/callback", api.CallbackHandler)
+	}
 
 	return r.Run(":8080")
 }
